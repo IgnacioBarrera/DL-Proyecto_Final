b0954322b9ac420dd9ef3e7f1fcb3a79c377d802
Your Name <you@example.com>
2020-12-16 00:01:48 -0300

    arreglos del navbar y otros detalles

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<template>
-  <div id="app">
+<template >
+  <div id="app" style="background-color: #734046;">
     <div>
       <b-navbar toggleable="lg" type="light" variant="white" class="navbar_borde">
-        <img src="@/assets/img/Logo.png" alt="" class="logo_minera-barba" />
+        
+        <b-navbar-brand><router-link to="/"><img src="@/assets/img/Logo.png" alt="" class="logo_minera-barba" /></router-link></b-navbar-brand>
+        <b-navbar-brand><router-link style="text-decoration: none"  to="/Contacto">Contacto</router-link></b-navbar-brand>
+        <b-navbar-brand><router-link style="text-decoration: none" :to="{name: 'TrabajaConNosotros'}">Trabaja Con Nosotros</router-link></b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item><router-link to="/">Inicio</router-link></b-nav-item>
-            <b-nav-item><router-link to="/Contacto">Contacto</router-link></b-nav-item>
-            <b-nav-item><router-link :to="{name: 'TrabajaConNosotros'}">Trabaja Con Nosotros</router-link></b-nav-item>
-
             <b-nav-item-dropdown right>
               <!-- Using 'button-content' slot -->
               <template #button-content>
@@ -25,6 +24,7 @@
       </b-navbar>
     </div>
     <router-view />
+    <footer class="text-center">Este seria el footer</footer>
   </div>
 </template>
 
@@ -35,6 +35,9 @@ export default {
 </script>
 
 <style scoped>
+  html {
+    background-color: #654062;
+  }
   .logo_minera-barba {
     width: 150px;
     height: 75px;
@@ -44,6 +47,16 @@ export default {
   }
   a,em{
     color: orange;
+  }
+  footer {
+    
+    background-color: #fff;
+    border-top: 2px solid orange;
+    height: 100px;
+    padding: 50px 0;
+  }
+  .linke {
+    text-decoration-color: none;
   }
 </style>
 
