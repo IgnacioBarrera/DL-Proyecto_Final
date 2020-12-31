b24e837481d07907b51d3592a2159ebf9f102ce9
Your Name <you@example.com>
2020-12-31 02:03:15 -0300

    llamando la coleccion de contactanos

--- src/store/store.js
+++ src/store/store.js
@@ -13,21 +13,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    contactenos: [],
     apiDatos: []
   },
   getters: {
     enviarApiDatos(state){
       return state.apiDatos;
+    },
+    /* Coleccion de contactenos */
+    enviarContacto(state) {
+      return state.contactenos;
     }
   },
   mutations: {
-
+    /* llamando Api */
     mutandoApiDatos (state, valores) {
       state.apiDatos = valores;
+    },
+
+    /* coleccion de contactenos */
+    mutarContacto(state, arreglo) {
+      state.contactenos = arreglo;
     }
   },
   actions: {
+
+    /* coleccion de trabaja con nosotros */
     enviandoTrabajo(context,form){
       db.collection('trabaja_con_nosotros').add({
         correo: form.correo,
@@ -40,6 +51,8 @@ export default new Vuex.Store({
       form.profesion = '';
       form.telefono = '';
     },
+
+    /* coleccion de contactenos */
     enviandoContacto(context,form){
       db.collection('contactenos').add({
         correo: form.correo,
@@ -50,6 +63,40 @@ export default new Vuex.Store({
       form.mensaje = '';
       form.nombre = '';
     },
+
+    traerContacto({commit}) {
+      db.collection('contactenos').onSnapshot(resp => {
+        let arreglo = [];
+        resp.forEach(element => {
+          arreglo.push({
+            id: element.id,
+            nombre: element.data().nombre,
+            correo: element.data().correo,
+            mensaje: element.data().mensaje,
+          });
+        });
+        commit('mutarContacto', arreglo);
+      })
+    },
+
+    actualizarContacto(context, contactos) {
+      db.collection('contactenos').doc(contactos.id).update({
+        nombre: contactos.nombre,
+        correo: contactos.correo,
+        mensaje: contactos.mensaje
+      })
+
+    },
+
+    eliminarContato(context, id) {
+      db.collection('contactenos').doc(id).delete().then(() => {
+        console.log('Eliminado');
+      }).catch(error => {
+        console.log(error);
+      })
+    },
+
+    
     /* llamando a la api */
     async traerApiDatos ({commit}) {
       let datos = await apiGet();
